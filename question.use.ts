146d1898bb96c6271a189931b24c86193b0c8efa
miralbhalani <miralbhalani@gmail.com>
2020-05-23 14:55:27 +0530

    Create question.use.ts

---
+++ question.use.ts
@@ -0,0 +1,7 @@
+let questionInstance1 = new Question();
+questionInstance1.setValue(5)
+console.log(questionInstance1.getValue())
+
+let questionInstance2 = new Question();
+questionInstance2.setValue(6)
+console.log(questionInstance2.getValue())
